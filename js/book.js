a22d65e57785c6b2fbd0caa73ba374e038f509ca
hmqqk <2442501113@qq.com>
2017-03-09 15:44:27 +0800

    sources

--- js/book.js
+++ js/book.js
@@ -2,28 +2,23 @@ document.write("<script type='text/javascript' src='js/jquery.js'></script>");
 document.write("<script type='text/javascript' src='js/jquery.md5.js'></script>");
 
 function book(ticketTypeID,TicketTypeIDDistributorLevelCode,ticketTypeName,num,touristName,touristTel,touristDate,check){
-       alert("ticketTypeName1="+ticketTypeName);
-       alert("进入Book函数");
-       alert("ticketTypeID="+ticketTypeID);//76
        var token1="3714861a5b5d78f9bd5d25cb25b371a1357";
-       alert("token1="+token1);
        //根据获取到的票型来看ID
+      //alert("check"+check);
        var ticketTypeID1=ticketTypeID;
-       alert("ticketTypeID1="+ticketTypeID1);
        var TicketTypeIDDistributorLevelCode1=TicketTypeIDDistributorLevelCode;
        var ticketTypeName1=ticketTypeName;
-       alert("ticketTypeName1="+ticketTypeName1);
        var strticketNum1=num;
-       var thirdpartyno1="123456789012";
+       var thirdpartyno1="1234567890127";
        var TourDate1=touristDate;
-       alert("touristDate="+touristDate);
+       alert("日期hahahah"+TourDate1);
        var touristName1=touristName; 
        var touristPhone1=touristTel;
        var MMSflag1=check;
        var bookorbuyorverifyflag1="1";
-
+       alert("what");
        jQuery.ajax({
-    		      url:"http://www.52uku.net/webservice.asmx/bookAndBuyTicketsForDistributor?jsoncallback=?",
+    		       url:"http://www.52uku.net/webservice.asmx/bookAndBuyTicketsForDistributor?jsoncallback=?",
                    type:"GET",   
                    contentType: "application/json", 
                    data:{
@@ -38,34 +33,19 @@ function book(ticketTypeID,TicketTypeIDDistributorLevelCode,ticketTypeName,num,t
                     "touristPhone":touristPhone1,
                     "MMSflag":MMSflag1,
                     "bookorbuyorverifyflag":bookorbuyorverifyflag1,
-                    
-                    "verifycode":$.md5(token1+ticketTypeID1+TicketTypeIDDistributorLevelCode1+ticketTypeName1+strticketNum1+thirdpartyno1+TourDate1+touristName1+touristPhone1+MMSflag1+bookorbuyorverifyflag1)
-                        },    //这里是要传递的参数，格式为data: "{paraName:paraValue}",
+                    "verifycode":$.md5(token1+ticketTypeID1+TicketTypeIDDistributorLevelCode1+ticketTypeName1+strticketNum1+thirdpartyno1+TourDate1+touristName1+touristPhone1+MMSflag1+bookorbuyorverifyflag1)},
                    dataType: "jsonp",
-                   jsonp:'callback',                          //服务器端获取回调函数名的key，对应后台有$_GET['callback']='getName';callback是默认值
-                   jsonpCallback:'jsonpCallback',                   //回调函数名
+                   jsonp:'callback',
+                   jsonpCallback:'jsonpCallback',
                    timeout:3000,
-                   //beforeSend:function(x) { x.setRequestHeader("Content-Type","application/json; charset=utf-8"); },
                  success: function (result) {
-                 	alert("result.STATUS="+result.STATUS);
-                 	alert("result.MESSAGE="+result.MESSAGE);
-                    alert("touristDate="+touristDate);
-                    alert("touristPhone1="+touristPhone1);
                  	if(result.STATUS=="OOOKK"){
                         alert("您已经购票成功");
-                        var oInfor=document.getElementById("buy_information");
-                        oInfor.style.display="none";
-                 		
-                 		
                  	}
-                 	
-                 
                  } ,
-                 	Error: function(err){
-                 	alert("ghfgh");
+                 	error: function(err){
+                 	alert("购票失败");
                  }
-                 
-                 
             })
        
        
